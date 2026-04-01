11b7e9d1e06629252226699e6809f6b580ed8550
rohan-dev77 <rohanrooswelt.27csb@licet.ac.in>
2026-04-01 08:39:03 +0530

    made text changes

--- src/main/java/com/example/demo/Controller/GreetingController.java
+++ src/main/java/com/example/demo/Controller/GreetingController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
     @GetMapping("/greet")
     public String greet() {
-        return "Hello Welcome";
+        return "Hello Wel";
     }
 }
